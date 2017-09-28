9b48502b436fd6e288f96b59b538c485d1a67ee2
irayt1063 <wrightigor@gmail.com>
2017-09-28 15:52:49 -0400

    Handling user's data

--- app/src/main/java/com/moment/UserInfoActivity.java
+++ app/src/main/java/com/moment/UserInfoActivity.java
@@ -4,6 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+
 
 public class UserInfoActivity extends AppCompatActivity {
 
@@ -15,8 +20,24 @@ public class UserInfoActivity extends AppCompatActivity {
     }
 
     public void btn_start_application_press(View view){
+        collectUserInformation();
         Intent user_info_intent = new Intent(this, MainActivity.class);
         startActivity(user_info_intent);
+
+    }
+
+    private String[] collectUserInformation(){
+        String[] userData = {};
+        EditText userName = (EditText)findViewById(R.id.txt_name);
+        EditText userEmail = (EditText)findViewById(R.id.txt_email);
+        SeekBar initialPhoneTime = (SeekBar)findViewById(R.id.seekbar_initial_time_for_phone);
+        SeekBar finalPhoneTime = (SeekBar)findViewById(R.id.seekbar_final_phone_time);
+        RadioGroup dataShareRadioGroup = (RadioGroup)findViewById(R.id.rad_group_data_share);
+        RadioButton dataShare = (RadioButton)  dataShareRadioGroup.getChildAt(idx);
+        String selectedtext = dataShare.getText().toString();
+
+        userData = {userName.getText(), userEmail.getText(), initialPhoneTime.}
+        return userData;
 
     }
 
