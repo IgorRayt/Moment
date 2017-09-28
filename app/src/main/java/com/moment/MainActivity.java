8147cda2b94e53724d70b4e6bf4a17059b972531
irayt1063 <wrightigor@gmail.com>
2017-09-28 18:09:10 -0400

    finished passing user data to main activity

--- app/src/main/java/com/moment/MainActivity.java
+++ app/src/main/java/com/moment/MainActivity.java
@@ -1,7 +1,12 @@
 package com.moment;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fillInUserData();
+    }
+
+    private void fillInUserData(){
+        Bundle extras = getIntent().getExtras();
+
+        TextView userNameLbl = (TextView)findViewById(R.id.lbl_name);
+        TextView userEmailLbl = (TextView)findViewById(R.id.lbl_user_email);
+        TextView usePhoneUseLbl = (TextView)findViewById(R.id.lbl_phone_use);
+        TextView userDataShareLbl = (TextView)findViewById(R.id.lbl_data_share);
+        String userNameLblText;
+        String userEmailLblText;
+        String phoneUseLblText;
+        String userDataShareLblText;
+
+        if (extras != null) {
+            String[] userData = extras.getStringArray("userDataArray");
+            String userName = userData[0];
+            String userEmail = userData[1];
+            String intialPhoneTime = userData[2];
+            String finalPhoneTime = userData[3];
+            String data_share_agreement = userData[4];
+
+            userNameLblText = String.format("Hello, %s, welcome to Moment Application",
+                    userName);
+            userEmailLblText = String.format("Your Email is %s", userEmail);
+            phoneUseLblText = String.format("Your current phone user is %s hours " +
+                    "per day. You target is %s hours per day", intialPhoneTime, finalPhoneTime);
+            if (data_share_agreement == "Yes") {
+                userDataShareLblText = "Thank you for sharing your data!";
+            }
+            else{
+                userDataShareLblText = "You declined the data share!";
+            }
+        }
+        else{
+            Context context = getApplicationContext();
+            CharSequence text = "Error!. No User Data has been passed! Restart app.";
+            int duration = Toast.LENGTH_LONG;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+
+            userNameLblText = "Error! User Name hasn't been provided!";
+            userEmailLblText = "Error! User Email hasn't been provided!";
+            phoneUseLblText = "Error! Phone use time hasn't been provided";
+            userDataShareLblText = "Error! Data share confirmation hasn't been provided!";
+        }
+
+        userNameLbl.setText(userNameLblText);
+        userEmailLbl.setText(userEmailLblText);
+        usePhoneUseLbl.setText(phoneUseLblText);
+        userDataShareLbl.setText(userDataShareLblText);
+
+
     }
 }
--- app/src/main/java/com/moment/UserInfoActivity.java
+++ app/src/main/java/com/moment/UserInfoActivity.java
@@ -8,6 +8,7 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 
 public class UserInfoActivity extends AppCompatActivity {
@@ -17,28 +18,111 @@ public class UserInfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
 
+        SeekBar initialPhoneTimeSeekBar =
+                (SeekBar)findViewById(R.id.seekbar_initial_time_for_phone);
+        SeekBar finalPhoneTimeSeekBar = (SeekBar)findViewById(R.id.seekbar_final_phone_time);
+        final TextView lbl_initialPhoneTimeSeekBar =
+                (TextView)findViewById(R.id.lbl_seekbar_initial_time_for_phone);
+        final TextView lbl_finalPhoneTimeSeekBar =
+                (TextView)findViewById(R.id.lbl_seekbar_final_phone_time);
+        initialPhoneTimeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {
+                String text;
+                if (progress == 1){
+                    text = "1 hour";
+                }
+                else{
+                    text = String.format("%s hours", Integer.toString(progress));
+                }
+                lbl_initialPhoneTimeSeekBar.setText(text);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                int progress = seekBar.getProgress();
+                String text;
+                if (progress == 1){
+                    text = "1 hour";
+                }
+                else{
+                    text = String.format("%s hours", Integer.toString(progress));
+                }
+                lbl_initialPhoneTimeSeekBar.setText(text);
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        finalPhoneTimeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {
+                String text;
+                if (progress == 1){
+                    text = "1 hour";
+                }
+                else{
+                    text = String.format("%s hours", Integer.toString(progress));
+                }
+                lbl_finalPhoneTimeSeekBar.setText(text);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                int progress = seekBar.getProgress();
+                String text;
+                if (progress == 1){
+                    text = "1 hour";
+                }
+                else{
+                    text = String.format("%s hours", Integer.toString(progress));
+                }
+                lbl_finalPhoneTimeSeekBar.setText(text);
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 
     public void btn_start_application_press(View view){
         collectUserInformation();
         Intent user_info_intent = new Intent(this, MainActivity.class);
+        user_info_intent.putExtra("userDataArray", collectUserInformation());
         startActivity(user_info_intent);
 
     }
 
     private String[] collectUserInformation(){
-        String[] userData = {};
-        EditText userName = (EditText)findViewById(R.id.txt_name);
-        EditText userEmail = (EditText)findViewById(R.id.txt_email);
-        SeekBar initialPhoneTime = (SeekBar)findViewById(R.id.seekbar_initial_time_for_phone);
-        SeekBar finalPhoneTime = (SeekBar)findViewById(R.id.seekbar_final_phone_time);
+        String[] userData;
+        EditText userNameTextField = (EditText)findViewById(R.id.txt_name);
+        EditText userEmailTextField = (EditText)findViewById(R.id.txt_email);
+        SeekBar initialPhoneTimeSeekBar =
+                (SeekBar)findViewById(R.id.seekbar_initial_time_for_phone);
+        SeekBar finalPhoneTimeSeekBar = (SeekBar)findViewById(R.id.seekbar_final_phone_time);
         RadioGroup dataShareRadioGroup = (RadioGroup)findViewById(R.id.rad_group_data_share);
-        RadioButton dataShare = (RadioButton)  dataShareRadioGroup.getChildAt(idx);
-        String selectedtext = dataShare.getText().toString();
+        String data_share_agreement =
+                ((RadioButton)findViewById(dataShareRadioGroup.getCheckedRadioButtonId()))
+                        .getText().toString();
 
-        userData = {userName.getText(), userEmail.getText(), initialPhoneTime.}
+        int initialPhoneTime = initialPhoneTimeSeekBar.getProgress();
+        int finalPhoneTime = finalPhoneTimeSeekBar.getProgress();
+        String userName = userNameTextField.getText().toString();
+
+        userData = new String[]{userNameTextField.getText().toString(),
+                userEmailTextField.getText().toString(), Integer.toString(initialPhoneTime),
+                Integer.toString(finalPhoneTime), data_share_agreement};
         return userData;
 
     }
+
 
 }
